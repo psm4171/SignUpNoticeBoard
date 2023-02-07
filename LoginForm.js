80b3f46428afc1597a2f9d158ea750d228594239
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-07 20:20:02 +0900

    Update LoginForm.js

--- LoginForm.js
+++ LoginForm.js
@@ -1,3 +1,4 @@
+// LoginForm.js 
 import React, { Component } from "react"; 
 import { Form, Button, Navbar, NavLink } from "react-bootstrap"; 
 import axios from "axios";
@@ -16,10 +17,10 @@ class LoginForm extends Component {
         buttonDisplay : "none"
     };
 
-    // 회원가입 실행하는 함수 
+    // 회원가입 실행하는 부분  
     join = () => {
         const send_param = {
-            headers,
+            headers, // 통신할 때마다 headers 반환 
             email: this.joinEmail.value,
             name: this.joinName.value,
             password: this.joinPw.value
@@ -39,10 +40,39 @@ class LoginForm extends Component {
                 console.log(err);
             });
     }; 
+
+    // 로그인 실행하는 부분 
+    login = () => {
+        const send_param = {
+            headers, 
+            email: this.loginEmail.value,
+            password: this.loginPw.value
+        };
+
+        axios   
+            .post("http://localhost:8080/member/login", send_param)
+            .then(returnData => {
+                if (returnData.data.message){
+                    // console.log("log_id: " + returnData.data._id); 
+                    $.cookie("login_id", returnData.data._id);
+
+                    alert(returnData.data.message);
+                    window.location.reload(); 
+                }
+                else{
+                    alert("로그인 실패"); 
+                }
+            })
+
+            .catch(err => {
+                console.log(err);
+            });
+
+    };
     
     // css style 부분은 render() 안에 
     render() {
-        const fromStyle={
+        const formStyle={
             margin: 50
         }; 
         const buttonStyle={
@@ -78,7 +108,6 @@ class LoginForm extends Component {
                     </Button>
                     </Form.Group>
 
-
                     <Form.Group controlId="loginForm">
                         <Form.Label>Email address</Form.Label>
                     <Form.Control
@@ -92,7 +121,7 @@ class LoginForm extends Component {
                         ref={ref => (this.joinPw = ref)}
                         placeholder="Password"
                     />
-                    <Button style={buttonStyle} onClick={this.join} variant="primary" type="button" block>
+                    <Button style={buttonStyle} onClick={this.login} variant="primary" type="button" block>
                         로그인 
                     </Button>
                     </Form.Group>
@@ -102,75 +131,3 @@ class LoginForm extends Component {
 }
 
 export default LoginForm;
-
-
-
-/*
-            <div>
-                <Navbar>
-                    <Navbar.Brand href="/">Today I Learned</Navbar.Brand>
-                    <Navbar.Toggle/>
-                    <Navbar.Collapse className="justify-content-end">
-                        <NavLink to="/">
-                            <Button style={buttonStyle} variant="primary">글 목록</Button>
-                        </NavLink>
-                        <NavLink to="/boardWrite">
-                            <Button stlye={buttonStyle} variant="primary">글 쓰기</Button>
-                        </NavLink>
-                        <Button stlye={buttonStyle} onClick={this.logout} variant="primary">
-                            로그아웃
-                        </Button>
-                        </Navbar.Collapse>
-                </Navbar>
-                    <Image src="./img/main.png" fluid/>
-            </div>
-        );
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form> 
-        )
-    }
-
-    // 로그인 실행하는 함수 
-    login = () => {
-        const send_param = {
-            headers,
-            email: this.loginEmail.value,
-            password: this.loginPw.value
-        };
-
-        axios
-            .post("http://localhost:8080/member/login", send_param)
-            .then(returnData => {
-                if(returnData.data.message){
-                   // console.log("login_id : " + returnData.data._id); 
-                    $.cookie("login_id", returnData.data._id); // 쿠키에 로그인 id가 있으면 로그인 처리 
-                alert(returnData.data.message);
-                window.location.reload();
-            }else {
-                alert("로그인 실패");
-            }
-            })
-
-    }
-}
-*/
