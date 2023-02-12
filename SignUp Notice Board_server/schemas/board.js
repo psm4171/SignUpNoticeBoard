5b0c136dabc91ec68eb12b0c17d0577870c65ff7
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-12 23:59:55 +0900

    Update board.js

--- SignUp Notice Board_server/schemas/board.js
+++ SignUp Notice Board_server/schemas/board.js
@@ -1,1 +1,33 @@
+const mongoose = require("mongoose"); 
 
+const {Schema} = mongoose;
+const {
+    Types: {ObjectId} 
+} = Schema; 
+
+const boardSchema = new Schema({
+    writer: {
+        type: ObjectId,
+        required: true, // 무조건 필수적으로 값을 받음 not null 
+        ref: "User" // User를 참조하여 사용 
+    },
+
+    title: {
+        type: String, 
+        required: true
+    },
+
+    content: {
+        type: String, 
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now // 현재 시간을 기본값으로 
+    }
+
+
+});
+
+module.exports = mongoose.model("Board", boardSchema);
