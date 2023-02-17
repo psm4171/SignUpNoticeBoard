be1c766f6d4791ec26bd78d4c342feb49d839908
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-18 00:00:17 +0900

    Update body.js

--- SignUp Notice Board_s/src/body.js
+++ SignUp Notice Board_s/src/body.js
@@ -4,6 +4,21 @@ import $ from "jquery";
 import {} from "jquery.cookie"; 
 
 class Body extends Component {
+    render(){
+        let resultForm; 
+        function getResulForm(){
+         
+            if($.cookie("login_id")){
+                // 쿠키값이 있으면 글쓰기 목록 리스트가 나오는 화면 게시글 목록 리스트가 나오는 화면을 넘김  
+                resulForm = <Route exact path="/" component={BoardForm}> </Route>; 
+                return resultForm; 
+            }else{
+                resultForm = <Route exact path="/" component={loginForm}></Route>;
+                return resultForm;
+            }
+        }
+        getResultForm();
+    }
 
     
 }
