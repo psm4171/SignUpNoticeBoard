8c46921c9315243e703e10509e91847953af20fe
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-19 23:53:45 +0900

    Update index.js

--- SignUp Notice Board_s/src/index.js
+++ SignUp Notice Board_s/src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {HashRouter} from 'react-router-dom'; 
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"; // 부트스트랩 css import 
 
+ReactDOM.render (
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+      <Header/>
+      <Body/>
+      <Footer/> 
+    </HashRouter>,
+    document.querySelector("#container")
+  
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
