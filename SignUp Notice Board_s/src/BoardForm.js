b82facefb088cf2026b590c2a5508828a2e2b77a
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-21 23:58:44 +0900

    Create BoardForm.js

---
+++ SignUp Notice Board_s/src/BoardForm.js
@@ -0,0 +1,24 @@
+// 글목록 리스트를 보여주는 곳 
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+import axios from "axios"; 
+import $ from "jquery"; 
+import {} from "jquery.cookie"; 
+axios.defaults.withCredentials = true;
+const headers = { withCredentials: true};
+
+class BoardRow extends Component{
+
+
+
+
+    redner(){
+
+        return(
+
+
+
+        );
+    }
+}
