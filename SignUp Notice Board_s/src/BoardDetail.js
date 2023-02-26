deb262dcad018b7d32d1c82e95d7a93b5e74ef53
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-27 00:00:20 +0900

    Update BoardDetail.js

--- SignUp Notice Board_s/src/BoardDetail.js
+++ SignUp Notice Board_s/src/BoardDetail.js
@@ -12,5 +12,36 @@ class BoardDetail extends Component {
         board: []
     }; 
 
+    // 생명주기에서 렌더링이 다 되었을 때 호출되는 함수 
+    componentDidMount(){
+        if(this.props.location.query !== undefined){
+            this.getDetail();
+        }else {
+            window.location.href="/";
+        }
+    }
+
+    deleteBoard = _id => {
+
+    }
     
+    getDetail = () => {
+        const send_param = {
+            headers, 
+            _id: this.props.location.query._id
+            // _id로 게시글을 가져옴 
+        };
+
+        axios
+            .post("http://localhost:8080/board/detail", send_param)
+            .then(returnData => {
+                if(returnData.data.board[0]){
+                    const board = (
+                        <div>
+                            <Table 
+                        </div>
+                    )
+                }
+            })
+    }
 }
