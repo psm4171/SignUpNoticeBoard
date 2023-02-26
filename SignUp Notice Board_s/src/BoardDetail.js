20e09f4ed0b5d2e1e63e474794ccbf0357d9bc47
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-02-27 00:22:59 +0900

    Update BoardDetail.js

--- SignUp Notice Board_s/src/BoardDetail.js
+++ SignUp Notice Board_s/src/BoardDetail.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 import axios from "axios"; 
-import $ from "jquery"; 
-import {} from "jquery.cookie"; 
 axios.defaults.withCredentials = true;
 const headers = { withCredentials: true};
 
@@ -22,8 +20,23 @@ class BoardDetail extends Component {
     }
 
     deleteBoard = _id => {
-
-    }
+        const send_param = {
+            headers, 
+            _id 
+        }; 
+            if(window.confirm("정말 삭제하시겠습니까?")){
+                axios
+                    .post("http://localhost:8080/board/delete", send_param)
+                    .then(returnData => {
+                        alert("게시글이 삭제 되었습니다."); 
+                        window.location.href="/";
+                    })
+                    .catch(err=> {
+                        console.log(err);
+                        alert("글 삭제 실패"); 
+                    });
+            }
+    };
     
     getDetail = () => {
         const send_param = {
@@ -35,13 +48,60 @@ class BoardDetail extends Component {
         axios
             .post("http://localhost:8080/board/detail", send_param)
             .then(returnData => {
-                if(returnData.data.board[0]){
+                if(returnData.data.board[0]){ // 데이터 상세 페이지 1개만 존재하기 때문에 [0]
                     const board = (
                         <div>
-                            <Table 
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th> {returnData.data.board[0].title}</th> 
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td dangerouslySetInnerHTML={{
+                                            __html: returnData.data.board[0].content
+                                        }} // 태그를 이용해서 html이 성공적으로 반영할 수 있도록 설정 
+                                        ></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            <div>
+                                <NavLink 
+                                to={{ pathname: "/boardWrite", query: { title: returnData.data.board[0].title, content: returnData.data.board[0].content, _id: this.props.location.query._id }}}
+                                >
+                                    <Button>글 수정</Button>
+                                </NavLink>
+                                        <Button 
+                                            onClick={this.deleteBoard.bind(
+                                                null, 
+                                                this.props.location.query._id
+                                                )}
+                                                >
+                                                    글 삭제 
+                                                </Button>
+                            </div>
                         </div>
-                    )
+                    );
+                    this.setState({
+                        board: board //state에 값을 세팅 
+                    });
+                }else {
+                    alert("글 상세 조회 실패");
                 }
             })
+            .catch(err => {
+                console.log(err); 
+            });
+
+    };
+
+    render() {
+        const divStyle = {
+            margin: 50
+        }; 
+        return <div style={divStyle}> {this.state.board}</div>
     }
 }
+
+export default BoardDetail;
