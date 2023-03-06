5fe92261dbc492bbf24ed638eb2db2acfe88d6ca
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-03-07 00:28:11 +0900

    Update memberRouter.js

--- SignUp Notice Board_server/memberRouter.js
+++ SignUp Notice Board_server/memberRouter.js
@@ -1,4 +1,3 @@
-
 const express = require("express"); 
 const router = express.Router(); 
 const User = require("../schemas/user"); 
@@ -32,7 +31,7 @@ router.post("/login", async (req, res) => {
 
         const user = await User.find(obj); // 이메일과 패스워드가 일치하면 user 변수를 찾음 
         console.log(user[0]); // 아이디 패스워드 일치한 1개의 배열 
-        if(user[0]) { // 일치한 배열을 찾으면 로그인 완료 표시
+        if(user[0]) {
             console.log(req.body._id); 
             res.json({message: "로그인 되었습니다!" , _id: user[0]._id});
         }else {
@@ -44,4 +43,6 @@ router.post("/login", async (req, res) => {
         res.json({message: false}); 
     }
 
-}); 
+});
+
+module.exports = router; // 라우터 내보내기가 존재해야 서버가 정상적으로 작동 
