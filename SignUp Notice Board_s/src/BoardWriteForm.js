7ec6274359044d52cb4156313bb472c4a1002968
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-03-07 00:00:55 +0900

    Update BoardWriteForm.js

--- SignUp Notice Board_s/src/BoardWriteForm.js
+++ SignUp Notice Board_s/src/BoardWriteForm.js
@@ -1,1 +1,11 @@
 import React, {Component} from "react";
+import { Button, From} from "react-bootstrap";
+import axios from "axios"; 
+import $ from "jquery"; 
+
+
+class BoardWriteForm extends Component{
+    state = {
+        data: ""
+    };
+}
